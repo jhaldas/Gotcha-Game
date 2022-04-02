049f5a3d863e26c1f157ff5af41289e3f8433cd2
Jhaldy <jonathan.haldas@gmail.com>
2022-04-01 22:32:34 -0400

    Fixed Tylers trash code lol

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-    public float speed = 40f;
+    public float speed = 1f;
     float horizontalMove = 0f;
     bool jump = false;
 
     void Update()
     {
-        horizontalMove = Input.GetAxis("Horizontal") * speed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             jump = true;
         }
+
+        controller.Move(horizontalMove, false, jump);
+        jump = false;
     }
 
     void FixedUpdate()
     {
-        controller.Move(5f, false, jump);
-        jump = false;
+
+        
     }
 }
